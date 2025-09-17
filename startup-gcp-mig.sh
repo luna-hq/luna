902f1a9197b6b1ede96f9b896ff1b35d0ee95917
flowerinthenight <root@flowerinthenight.com>
2025-09-17 11:28:37 +0900

    ci skip: update svc desc

--- startup-gcp-mig.sh
+++ startup-gcp-mig.sh
@@ -11,7 +11,7 @@ chown root:root /usr/local/bin/luna
 
 cat >/usr/lib/systemd/system/luna.service <<EOL
 [Unit]
-Description=Luna - in-memory, columnar SQL server
+Description=Luna - in-memory SQL server
 
 [Service]
 Type=simple
